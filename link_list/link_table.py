c06a28853d80c35d0bc99f662224e1a627b4a29d
RyoNak <nakagamiryo0901@gmail.com>
2025-04-16 21:08:25 +0900

    UPDATE POST: complex number and inner-product

---
+++ link_list/link_table.py
@@ -0,0 +1,44 @@
+import pathlib
+from typing import Dict, List
+from datetime import date
+import yaml
+from pydantic import BaseModel
+
+
+class KeywordField(BaseModel):
+    genre: str
+    comment: str
+    url: str
+
+
+class Config(BaseModel):
+    keywords: Dict[str | int, KeywordField]
+
+
+def new_config(paths_to_config: List[pathlib.Path]) -> Config:
+    """Generates a Config object from a yaml file.
+
+    Args:
+        path_to_config (str): Path to the configuration file.
+
+    Returns:
+        Config: A Config object.
+
+    """
+
+    merged_config = {}
+
+    for path in paths_to_config:
+        with open(path, encoding="utf-8") as f:
+            yaml_config = yaml.safe_load(f)
+            merged_config.update(yaml_config)
+    config: Config = Config.model_validate(merged_config)
+
+    return config
+
+
+# Define multiple config paths
+CONFIG_FILES = [
+    pathlib.Path(__file__).parent / "link.yml",
+]
+CONFIG: Config = new_config(paths_to_config=CONFIG_FILES)
